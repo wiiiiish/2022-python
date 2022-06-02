832e2558052130e26d9d6f9928e4b0ffa06bc9a1
김소원 <a90614@naver.com>
2022-06-02 18:07:13 +0900

    자료구조론 Day1

---
+++ python_0602/ex01.py
@@ -0,0 +1,13 @@
+# 여러 데이터를 저장한다
+list1 = [1, 2]
+int1 = 0
+float1 = 3.14
+set1 = set()
+tuple1 = (2, 3)
+
+# 저장한 데이터의 메모리 저장 위치를 받아온다
+print(id(list1))
+print(id(int1))
+print(id(float1))
+print(id(set1))
+print(id(tuple1))
