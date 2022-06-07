b2a5da6617f0e80fd5da80502299327acb9f2fc5
김소원 <a90614@naver.com>
2022-06-07 19:58:42 +0900

    Linked List 링크드 리스트 가장 앞에 데이터 삽입 실습

---
+++ python_0607/ex04.py
@@ -0,0 +1,102 @@
+class Node:
+    """링크드 리스트의 노드 클래스"""
+
+    def __init__(self, data):
+        self.data = data  # 노드가 저장하는 데이터
+        self.next = None  # 다음 노드에 대한 레퍼런스
+
+class LinkedList:
+    """링크드  리스트 클래스"""
+
+    def __init__(self):
+        self.head = None  # 링크드 리스트의 가장 앞 노드
+        self.tail = None  # 링크드 리스트의 가장 뒤 노드
+
+    def prepend(self, data):
+        """링크드 리스트의 가장 앞에 데이터 삽입"""
+        new_node = Node(data)
+
+        # 링크드 리스트가 비어 있으면 새로운 노드가 링크드 리스트의 처음이자 마지막 노드다
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        # 링크드 리스트가 비어 있지 않으면
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def insert_after(self, previous_node, data):
+        """링크드 리스트 주어진 노드 뒤 삽입 연산 메소드"""
+        new_node = Node(data)
+
+        # tail 노드 다음에 new_node를 추가할 때
+        if previous_node is self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        # 두 노드 사이에 new_node를 추가할 때
+        else :
+            new_node.next = previous_node.next
+            previous_node.next = new_node
+
+    def find_node_at(self, index):
+        """링크드 리스트 접근 연산 메소드. 파라미터 인덱스는 항상 있다고 가정"""
+        iterator = self.head
+
+        for _ in range(index):
+            iterator = iterator.next
+        return iterator
+
+    def append(self, data):
+        """링크드 리스트 추가 연산 메소드"""
+        new_node = Node(data)
+
+        # 링크드 리스트가 비어 있으면 새로운 노드가 링크드 리스트의 처음이자 마지막 노드다
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        # 링크드 리스트가 비어 있지 않으면
+        else:
+            self.tail.next = new_node  # 가장 마지막 노드 뒤에 새로운 노드를 추가하고
+            self.tail = new_node  # 마지막 노드를 추가한 노드로 바꿔준다
+
+    def __str__(self):
+        """링크드 리스트를 문자열로 표현해서 리턴하는 메소드"""
+        res_str = "|"
+
+        # 링크드  리스트 안에 모든 노드를 돌기 위한 변수. 일단 가장 앞 노드로 정의한다.
+        iterator = self.head
+
+        # 링크드  리스트 끝까지 돈다
+        while iterator is not None:
+            # 각 노드의 데이터를 리턴하는 문자열에 더해준다
+            res_str += f" {iterator.data} |"
+            iterator = iterator.next  # 다음 노드로 넘어간다
+
+        return res_str
+
+    def find_node_with_data(self, data):
+        """링크드 리스트에서 탐색 연산 메소드. 단, 해당 노드가 없으면 None을 리턴한다"""
+        iterator = self.head
+        while iterator is not None:
+            if iterator.data == data:
+                return iterator
+            else:
+                iterator = iterator.next
+        return None
+
+
+# 새로운 링크드 리스트 생성
+my_list = LinkedList()
+
+# 링크드 리스트에 데이터 추가
+my_list.prepend(11)
+my_list.prepend(7)
+my_list.prepend(5)
+my_list.prepend(3)
+my_list.prepend(2)
+
+print(my_list)
+
+# head, tail 노드가 제대로 설정됐는지 확인
+print(my_list.head.data)
+print(my_list.tail.data)
